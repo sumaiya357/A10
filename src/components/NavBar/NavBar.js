b4f7fe40f1c2871733feff273ef557a676abe00d
sumaiya246 <rahmansumaiya75@gmail.com>
2022-10-11 20:03:46 +0600

    nav bar added

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,19 @@
+ import React from 'react';
+ import {Link} from "react-router-dom";
+ 
+ const NavBar = () => {
+    return (
+        <div className='text-pink-600 m-8  flex flex-row justify-between  md:flex-row'>
+            <div>
+              <h3 className='text-2xl font-bold'> Takin' a Quiz</h3>
+            </div>
+            <div className='space-x-4'>
+                <Link to={'/'}>Home</Link>
+                <Link to={'/statistics'}>Statistics</Link>
+                <Link to={'/blog'}>Blog</Link>
+            </div>
+        </div>
+    );
+ };
+ 
+ export default NavBar;
